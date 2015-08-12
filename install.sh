87964e395233909a6a261748f5b9e478958521ac
LukeHarding <Harding.luke.aaron@gmail.com>
2015-08-12 11:01:00 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+logdir="/etc/syschanges"
+wherebe="$(pwd)"
+log="$logdir/current"
+loglocation="$log"
+if [ -d $logdir ]; then
+	echo "Already installed, yo. Chill out"
+	exit
+fi
+
+if [ "$(whoami)" != 'root' ]; then
+	echo "etc is not writeable by non-root accounts, exiting :( "
+	exit 1;
+fi
+
+echo "Creating directory for logs"
+mkdir $logdir
+sleep 2 # hahahahhahahahaha
+
+echo "adding syschanges to bin"
+cp $wherebe/syschanges /bin/
+chmod a+x /bin/syschanges
+
+echo "Generateing your current configuration."
+syschanges > $logdir/current
+
+echo "lspci:
+
+" > $loglocation
+lspci >> $loglocation
+echo "lsblk:
+
+" >> $loglocation
+lsblk >> $loglocation
+echo "lscpu:
+
+" >> $loglocation
+lscpu >> $loglocation
+echo "lsinitramfs:
+
+" >> $loglocation
+lsinitramfs /boot/initrd.img-$(uname --kernel-release) >> $loglocation
+
+echo "done."
